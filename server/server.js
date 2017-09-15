dfb698d2254087a663c6fe2d243aa1b926a149f6
viper1010 <viper.lannister@gmail.com>
2017-09-15 19:53:10 -0400

    Added the GET route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,13 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  })
+});
 
 app.listen(3000, ()=>{
   console.log('Todo Web Server started on port 3000!');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -52,5 +52,4 @@ describe('Post /todos', ()=>{
       });
 
   });
-
 });
