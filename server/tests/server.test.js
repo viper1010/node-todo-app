1d15da17476d46335df196a1e4568520fbba5d09
viper1010 <viper.lannister@gmail.com>
2017-09-15 20:04:19 -0400

    Added tests for GET route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,12 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
+const todos = [{text: 'First test todo'}, {text: 'Second test todo'}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(()=>done())
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=> done())
 })
 
 describe('Post /todos', ()=>{
@@ -25,7 +29,7 @@ describe('Post /todos', ()=>{
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -46,10 +50,22 @@ describe('Post /todos', ()=>{
         }
 
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e))
       });
-
   });
 });
+
+describe('GET /todos', ()=>{
+  it('should get all todos', (done) =>{
+
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done);
+  })
+})
